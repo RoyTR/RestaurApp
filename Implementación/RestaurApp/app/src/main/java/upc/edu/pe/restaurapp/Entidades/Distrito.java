a7f681c98fca8ef1452383fa3f6d37b883078a26
Andres Baquerizo <andres19292@gmail.com>
2015-06-13 19:15:26 -0500

    avances en la pestaña Buscar

---
+++ Implementación/RestaurApp/app/src/main/java/upc/edu/pe/restaurapp/Entidades/Distrito.java
@@ -0,0 +1,24 @@
+package upc.edu.pe.restaurapp.Entidades;
+
+public class Distrito {
+
+    private Integer id;
+    private String nombre;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
