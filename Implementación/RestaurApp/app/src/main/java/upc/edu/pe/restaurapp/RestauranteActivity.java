552c4f35deaabea144ad1f9465de24b1eff419c1
Andres Baquerizo <andres19292@gmail.com>
2015-06-22 01:17:07 -0500

    Recomendacion basica Ready

--- Implementación/RestaurApp/app/src/main/java/upc/edu/pe/restaurapp/RestauranteActivity.java
+++ Implementación/RestaurApp/app/src/main/java/upc/edu/pe/restaurapp/RestauranteActivity.java
@@ -1,6 +1,8 @@
 package upc.edu.pe.restaurapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.provider.MediaStore;
@@ -9,12 +11,28 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import upc.edu.pe.restaurapp.Entidades.Restaurante;
 
 
 public class RestauranteActivity extends ActionBarActivity {
 
     int IdRestaurante;
+    int recomendacion_grupoId;
     String Nombre ;
     String Latitud;
     String Longitud;
@@ -22,12 +40,19 @@ public class RestauranteActivity extends ActionBarActivity {
     Integer Foto_id;
     Double PuntuacionTotal;
     String Distrito;
+    ProgressDialog prgDialog;
+    public static final String RESTAURAPP_PREFERENCES = "RESTAURAPP_PREFERENCES";
+    public Integer idUsuarioLogueado;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurante_ver);
+
+        prgDialog = new ProgressDialog(this);
+        SharedPreferences shared = getSharedPreferences(RESTAURAPP_PREFERENCES, MODE_PRIVATE);
+        idUsuarioLogueado = shared.getInt("USUARIO_ACTUAL_ID", 0);
 
         //Action Bar personalizado
         final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
@@ -109,6 +134,9 @@ public class RestauranteActivity extends ActionBarActivity {
     public void irRecomendar(View v){
         setContentView(R.layout.activity_restaurante_recomendar);
 
+        Button btnRecomendar = (Button) findViewById(R.id.restaurante_btn_enviar_recomend);
+        btnRecomendar.setClickable(true);
+
         TextView txtNombre = (TextView) findViewById(R.id.restaurante_txt_nombre);
         TextView txtDescripcion = (TextView) findViewById(R.id.restaurante_txt_descripcion);
         TextView txtPuntaje = (TextView) findViewById(R.id.restaurante_txt_puntaje);
@@ -125,5 +153,83 @@ public class RestauranteActivity extends ActionBarActivity {
 
     }
 
+    //aqui va la funcion que envia al usuario a elegir un grupo o amigo
+    public void irAgregarAmigoGrupo(View v)
+    {
+        Intent intent = new Intent(this, AmigosGruposActivity.class);
+        startActivityForResult(intent, 1);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode)
+        {
+            case 1:
+                this.recomendacion_grupoId = data.getIntExtra("GrupoId",0);
+                break;
+        }
+    }
+
+    public void recomendarRestaurante(View v)
+    {
+        //sacar datos y llamar a la api para recomendar
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.restaurante_rec_ratingBar);
+        EditText comentarioET = (EditText) findViewById(R.id.restaurante_rec_comentarios);
+        float numEstrellas = ratingBar.getRating();
+        int puntuacion =  ((int)numEstrellas)*2;
+        String comentario = comentarioET.getText().toString();
+
+        //--------LOGICA DE LA LLAMADA A LA API----------//
+        AsyncHttpClient client = new AsyncHttpClient();
+        prgDialog.setMessage("Please wait...");
+        prgDialog.show();
+
+        RequestParams params = new RequestParams();
+        params.put("comentario", comentario);
+        params.put("puntuacion", puntuacion);
+        params.put("usuario_id", idUsuarioLogueado);
+        params.put("restaurante_id", this.IdRestaurante);
+        params.put("grupo_id", this.recomendacion_grupoId);
+        params.put("created_by", idUsuarioLogueado);
+
+        client.post("http://52.25.159.62/api/recomendaciones/create", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String response = new String(responseBody);
+                try {
+                    JSONObject obj = new JSONObject(response);
+                    //JSONObject obj2 = obj.getJSONObject("data");
+
+                    //TODO: revisar manejo del error
+                    if (response.contains("error")) {
+                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Recomendacion Enviada!", Toast.LENGTH_LONG).show();
+                    }
+                    Button btnRecomendar = (Button) findViewById(R.id.restaurante_btn_enviar_recomend);
+                    btnRecomendar.setClickable(false);
+                    prgDialog.hide();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                prgDialog.hide();
+                if (statusCode == 404) {
+                    Toast.makeText(getApplicationContext(), "No se encontro el resource", Toast.LENGTH_LONG).show();
+                } else if (statusCode == 500) {
+                    Toast.makeText(getApplicationContext(), "Hubo un error en el servidor", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Ocurrio un Error Inesperado [Puede que el dispositivo no esté conectado al Internet o que el servidor remoto no este funcionando]", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+        //--------FIN LOGICA DE LA LLAMADA A LA API-------//
+    }
 }
