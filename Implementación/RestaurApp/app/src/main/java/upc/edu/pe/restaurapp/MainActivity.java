a8d726ef22f9483d64fa249eb0637f273cc56753
rrcanales <rodrigo_canales@hotmail.com>
2015-07-05 17:51:25 -0500

    Agregada activity y funcion de revisar las recomendacions que le llegan al usuario

--- Implementación/RestaurApp/app/src/main/java/upc/edu/pe/restaurapp/MainActivity.java
+++ Implementación/RestaurApp/app/src/main/java/upc/edu/pe/restaurapp/MainActivity.java
@@ -103,6 +103,9 @@ public class MainActivity extends ActionBarActivity {
             case R.id.action_contactos:
                 irContactos(findViewById(R.id.action_contactos));
                 return true;
+            case R.id.action_recomendaciones:
+                irRecomendaciones(findViewById(R.id.action_recomendaciones));
+                return true;
             case R.id.action_about:
                 irAbout(findViewById(R.id.action_about));
                 return true;
@@ -241,6 +244,10 @@ public class MainActivity extends ActionBarActivity {
     //-----------------------------------MENU BAR----------------------------------//
     public void irContactos(View v){
         Intent intent = new Intent(this, ContactosActivity.class);
+        startActivity(intent);
+    }
+    public void irRecomendaciones(View v){
+        Intent intent = new Intent(this, RecomendacionesParaMiActivity.class);
         startActivity(intent);
     }
     public  void irUsuario(View v){
