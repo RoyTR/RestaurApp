bef47e88a05413af18162b496aa587b4c8c23ded
jsandovaln18 <jsandovaln18@gmail.com>
2015-07-06 11:39:16 -0500

    Integración Login con Facebook

--- Implementación/RestaurApp/app/src/main/java/upc/edu/pe/restaurapp/LoginMainActivity.java
+++ Implementación/RestaurApp/app/src/main/java/upc/edu/pe/restaurapp/LoginMainActivity.java
@@ -1,43 +1,78 @@
 package upc.edu.pe.restaurapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
-public class LoginMainActivity extends ActionBarActivity {
+import java.util.Arrays;
 
-    @Override
+public class LoginMainActivity extends Activity {
+
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private ProfileTracker profileTracker;
+    public static final String RESTAURAPP_PREFERENCES = "RESTAURAPP_PREFERENCES" ;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login_main);
-        getSupportActionBar().hide();
+        info = (TextView)findViewById(R.id.info);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday, user_friends"));
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "user_friends", "email", "user_birthday"));
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(
+                    Profile oldProfile,
+                    Profile currentProfile) {
+
+                if(currentProfile != null)
+                    info.setText(currentProfile.getName() + "-" + currentProfile.getName());
+            }
+        };
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                );
+            }
+
+
+            @Override
+            public void onCancel() {
+                info.setText("Problemas del servidor de Facebook.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Problemas del servidor de Facebook.");
+            }
+        });
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void Registro(View v){
